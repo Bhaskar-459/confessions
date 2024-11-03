caf548bda34d84c5f22547b59762fbbcb48c6e45
Bhaskar-459 <m03bhaskar@gmail.com>
2024-11-03 11:32:59 +0530

    Made the final changes

--- frontend/confessionfrontend/src/App.js
+++ frontend/confessionfrontend/src/App.js
@@ -3,13 +3,12 @@ import Login from './Login/Login';
 import Main from './Main/Main';
 import Credits from './Credits';
 import UserDetails from './UserDetails';
-import './App.css'; // Import the new CSS
+import './App.css';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    // Check local storage to see if the user is logged in
     const username = localStorage.getItem('username');
     if (username) {
       setIsLoggedIn(true);
@@ -24,9 +23,11 @@ function App() {
           "Sometimes the words we don't say are the ones that mean the most. Share your heart and let the silence speak."
         </p>
       </header>
-      <UserDetails />
       <div className="content">
-        {isLoggedIn ? <Main /> : <Login />}
+        {isLoggedIn ? <>
+          <Main />
+          <UserDetails />
+        </> : <Login />}
       </div>
       <Credits />
     </div>
